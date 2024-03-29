2fe07c44f4660e6e3c00015ba6988f694b69eb96
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2024-03-29 12:09:36 +0100

    simple tcp echo server

--- cmd/pustynia-server/main.go
+++ cmd/pustynia-server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	l, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go func() {
+			defer conn.Close()
+			b := make([]byte, 4096)
+			for {
+				n, err := conn.Read(b)
+				if err != nil {
+					if err != io.EOF {
+						log.Println(err)
+					}
+					return
+				}
+				if _, err = conn.Write(b[:n]); err != nil {
+					log.Println(err)
+					return
+				}
+			}
+		}()
+	}
 }
--- cmd/pustynia/main.go
+++ cmd/pustynia/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	stop := make(chan struct{})
+	go func() {
+		b := make([]byte, 4096)
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				n, err := conn.Read(b)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				fmt.Println(string(b[:n]))
+			}
+		}
+	}()
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		msg := s.Bytes()
+		if bytes.Equal(msg, []byte("exit")) {
+			close(stop)
+			break
+		}
+		if _, err = conn.Write(msg); err != nil {
+			log.Println(err)
+			break
+		}
+	}
+	if err = s.Err(); err != nil {
+		log.Fatalln(err)
+	}
 }
